db5561c1acdbb9dbb047fe0b1beaa53b7a9e6808
Juana <Juana@192.168.1.36>
2025-02-27 02:37:06 +0100

    Entidad User

---
+++ src/main/java/com/taskmanager/model/User.java
@@ -0,0 +1,32 @@
+package com.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+@Entity
+@Table (name="users")
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
