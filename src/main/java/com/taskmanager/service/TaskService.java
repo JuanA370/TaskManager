fcecb15d0f4f63786a266aa7022ab56052bb7792
Juana <Juana@192.168.1.36>
2025-02-28 03:14:51 +0100

    Nuevas dependencias WebSockets

--- src/main/java/com/taskmanager/service/TaskService.java
+++ src/main/java/com/taskmanager/service/TaskService.java
@@ -17,6 +17,9 @@ public class TaskService {
 	@Autowired
     private TaskRepository taskRepository;
 
+    @Autowired
+    private WebSocketNotificationService webSocketService;
+    
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
@@ -34,23 +37,28 @@ public class TaskService {
     }
 
     public Task createTask(Task task) {
-        task.setStatus(TaskStatus.PENDING);  // Estado inicial
-        task.setDueDate(task.getDueDate() != null ? task.getDueDate() : LocalDateTime.now().plusDays(3));  
-        return taskRepository.save(task);
+        task.setStatus(TaskStatus.PENDING);
+        task.setDueDate(task.getDueDate() != null ? task.getDueDate() : LocalDateTime.now().plusDays(3));
+        Task savedTask = taskRepository.save(task);
+        webSocketService.sendTaskUpdateNotification("Nueva tarea creada: " + task.getTitle());
+        return savedTask;
     }
 
     public Task updateTask(Long id, Task taskDetails) {
-        return taskRepository.findById(id).map(task -> {
-            task.setTitle(taskDetails.getTitle());
-            task.setDescription(taskDetails.getDescription());
-            task.setDueDate(taskDetails.getDueDate());
-            task.setPriority(taskDetails.getPriority());
-            task.setStatus(taskDetails.getStatus());
-            return taskRepository.save(task);
-        }).orElseThrow(() -> new RuntimeException("Task not found"));
+    	 return taskRepository.findById(id).map(task -> {
+             task.setTitle(taskDetails.getTitle());
+             task.setDescription(taskDetails.getDescription());
+             task.setDueDate(taskDetails.getDueDate());
+             task.setPriority(taskDetails.getPriority());
+             task.setStatus(taskDetails.getStatus());
+             Task updatedTask = taskRepository.save(task);
+             webSocketService.sendTaskUpdateNotification("Tarea actualizada: " + task.getTitle());
+             return updatedTask;
+         }).orElseThrow(() -> new RuntimeException("Tarea no encontrada"));
     }
 
     public void deleteTask(Long id) {
         taskRepository.deleteById(id);
+        webSocketService.sendTaskUpdateNotification("Una tarea ha sido eliminada.");
     }
 }
